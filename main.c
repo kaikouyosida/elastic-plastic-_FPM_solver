5706b3cefbedfea79e30992b36d324ede628dc86
gauss <7523568@ed.tus.ac.jp>
2024-02-08 13:50:47 +0900

    m

--- main.c
+++ main.c
@@ -7,9 +7,20 @@ Global global;
 Option option;
 SS_CURVE ss_curve;
 
+#define NON_LINEAR_SOLVER 1
+
+#define LINEAR_SOLVER 2
+
 int main(){
+    int solver_type = 1;
+
     Init_model();
-    analize_by_NewtonRapdon();
+
+    swith(solver_type){
+        case NON_LINEAR_SOLVER:
+            analize_by_NewtonRapdon();
+        break;
+    }
     
     printf("complete!\n");
     break_model_memory();
